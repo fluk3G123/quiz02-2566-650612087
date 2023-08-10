e3febf0a022501a7d0c5e36c101cf642bdf566ea
fluk3G123 <nitipong4434@gmail.com>
2023-08-10 15:34:15 +0700

    1.1.1

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,12 @@
 
 import { Comment } from "@/components/Comment";
 import { PostOwner } from "@/components/PostOwner";
+import { comments } from "@/libs/comments";
+import { Reply } from "@/components/Reply";
+import { useState } from "react";
 
 export default function HomePage() {
+  const [Comments, setComents] = useState([]);
   return (
     <div
       style={{ minHeight: "100vh", backgroundColor: "#18191A" }}
@@ -23,55 +27,23 @@ export default function HomePage() {
         />
 
         {/* Comment Example */}
-        {/* <Comment /> */}
-        {/* <div className="d-flex gap-2 my-2">
-          <img
-            src="/profileImages/lisa.jpg"
-            width="48"
-            height="48"
-            className="rounded-circle"
-            style={{ objectFit: "cover" }}
-          />
-          <div
-            className="rounded rounded-3 p-2"
-            style={{ backgroundColor: "#3A3B3C" }}
-          >
-            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
-              Lisa
-            </span>
-            <br />
-            <span style={{ color: "#E4E6EB" }}>จริงค่า</span>
-            <div className="d-flex align-items-center gap-1">
-              <img src="/like.svg" width={20}></img>
-              <span style={{ color: "#B0B3B8" }}>999 คน</span>
-            </div>
-          </div>
-        </div> */}
+        {}
+        <Comment
+          userImagePath={comments.userImagePath}
+          username={comments.username}
+          commentText={comments.replyText}
+          likeNum={comments.likeNum}
+        />
 
         {/* Reply Example */}
-        <div className="d-flex gap-2 my-2 ps-5">
-          <img
-            src="/profileImages/puppy.jpg"
-            width="48"
-            height="48"
-            className="rounded-circle"
-            style={{ objectFit: "cover" }}
-          />
-          <div
-            className="rounded rounded-3 p-2"
-            style={{ backgroundColor: "#3A3B3C" }}
-          >
-            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
-              หมาน้อย
-            </span>
-            <br />
-            <span style={{ color: "#E4E6EB" }}>จริงค้าบบบบบบบบ</span>
-            <div className="d-flex align-items-center gap-1">
-              <img src="/like.svg" width={20}></img>
-              <span style={{ color: "#B0B3B8" }}>2 คน</span>
-            </div>
-          </div>
-        </div>
+        <Reply />
+        <Reply />
+        <Comment
+          userImagePath={comments.userImagePath}
+          username={comments.username}
+          commentText={comments.replyText}
+          likeNum={comments.likeNum}
+        />
 
         {/* map-loop render Comment component here */}
       </div>
