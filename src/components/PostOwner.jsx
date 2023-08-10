03e8738609f80af2c0e2f779b8bdf458b04d91e6
fluk3G123 <nitipong4434@gmail.com>
2023-08-10 14:59:55 +0700

    1

--- src/components/PostOwner.jsx
+++ src/components/PostOwner.jsx
@@ -1,3 +1,24 @@
-export const PostOwner = ({}) => {
-  return <div>Your code here...</div>;
+export const PostOwner = ({ userImagePost, UserNamePost, Post, Like }) => {
+  return (
+    <div className="vstack gap-3">
+      <div className="d-flex align-items-center gap-3">
+        <img
+          src={userImagePost}
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <span className="fw-semibold fs-5 text-white">{UserNamePost}</span>
+      </div>
+
+      <span className="text-white">{Post}</span>
+
+      <div className="d-flex align-items-center gap-1">
+        <img src="/like.svg" width={20}></img>
+        <span style={{ color: "#B0B3B8" }}>{Like}</span>
+      </div>
+      <hr className="m-0 border" />
+    </div>
+  );
 };
